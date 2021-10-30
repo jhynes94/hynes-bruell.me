7178c0dc3545f50abbecd0bdc201bc58920c4a77
Justin <jbruell94@gmail.com>
2021-10-29 21:57:28 -0400

    Vue3 Repo Created

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+      <h2>Hello! Nav bar here!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
---
+++ src/components/Snake.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Snake App Here</h1>
+    <hr>
+    <p>Plz be working</p>
+    <p>Let's try again, and again</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SnakeApp',
+}
+</script>
