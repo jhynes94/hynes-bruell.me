d4ec0fc7ca5333ca57fa863022923c7b69f3a88f
Justin <jbruell94@gmail.com>
2021-11-10 16:28:04 -0500

    Flight update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <NavBar/>
     <h1>3D Rendering here</h1>
     <SnakeApp/>
   </div>
@@ -8,14 +7,12 @@
 
 <script>
 // @ is an alias to /src
-import NavBar from '@/components/NavBar.vue'
 import SnakeApp from '@/components/Snake.vue'
 
 export default {
   name: 'Home',
   components: {
     SnakeApp,
-    NavBar
   } 
 }
 </script>
